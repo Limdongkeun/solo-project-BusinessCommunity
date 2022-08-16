263d5d58d2059b3716068af17bd6215c5aa65d70
Dong Keun LIM <86710265+Limdongkeun@users.noreply.github.com>
2022-08-17 02:09:49 +0900

    Add files via upload

---
+++ BusinessCommunity/src/main/java/com/codestates/soloproject/BusinessCommunity/BusinessCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.codestates.soloproject.BusinessCommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessCommunityApplication.class, args);
+	}
+
+}
---
+++ BusinessCommunity/src/test/java/com/codestates/soloproject/BusinessCommunity/BusinessCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.soloproject.BusinessCommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
